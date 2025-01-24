d528d97afb26f18a2e1c060481c2722a8c83004c
Yaroslav Homziak <ywgk0102@gmail.com>
2025-01-24 21:00:45 +0200

    feat (location builder & game scene) : added location builder and main game scene

---
+++ Assets/TapToStep/Scripts/Runtime/Builders/Location/LocationGenerator.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Runtime.Builders.Location;
+using SO.Location.Logic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Runtime.Service.LocationGenerator
+{
+    public class LocationGenerator: MonoBehaviour, ILocationGenerator
+    {
+        [Header("Locations")]
+        [SerializeField] private List<SwitchedLocationSO> _supportedLocationPull;
+        [SerializeField] private Transform _locationParent;
+        
+        [Header("Generation Settings")]
+        [Range(1, 100)]
+        [SerializeField] private int _maxLocationLength = 1;
+        
+        private List<GameObject> _locationElementHoldersPull;
+        private Vector3 _locationGenerationPoint;
+        private bool _isFirstGeneration = true;
+        
+        public async UniTask GenerateNewLocationAsync()
+        {
+            var randomLocationIndex = Random.Range(0, _supportedLocationPull.Count);
+            
+            if (_isFirstGeneration)
+            {
+                _isFirstGeneration = !_isFirstGeneration;
+                await CreateLocationAsync(_supportedLocationPull[0], 20);
+                await CreateLocationAsync(_supportedLocationPull[randomLocationIndex], 20);
+            }
+            else
+            {
+                await CreateLocationAsync(_supportedLocationPull[randomLocationIndex]);
+                RemoveOldLocation();
+            }
+        }
+
+        private async UniTask CreateLocationAsync(SwitchedLocationSO locationSo, int millisDelay = 500)
+        {
+            var elementQueue = locationSo.GetLocations(_maxLocationLength);
+            var startLocationSpawnPosition = _locationGenerationPoint;
+            var locationElementsHolder = new GameObject($"{nameof(locationSo)}");
+            locationElementsHolder.transform.SetParent(_locationParent);
+            
+            while (elementQueue.Count > 0)
+            {
+                var temp = Instantiate(elementQueue.Dequeue(), startLocationSpawnPosition, Quaternion.identity);
+                temp.transform.SetParent(locationElementsHolder.transform);
+                await UniTask.Delay(millisDelay);
+
+                startLocationSpawnPosition = 
+                    new Vector3(
+                        startLocationSpawnPosition.x,
+                        startLocationSpawnPosition.y, 
+                        startLocationSpawnPosition.z + locationSo.LocationOffset);
+            }
+            
+            _locationElementHoldersPull.Add(locationElementsHolder);
+            _locationGenerationPoint = startLocationSpawnPosition;
+        }
+
+        private void RemoveOldLocation()
+        {
+            if (_locationElementHoldersPull.Count > 3)
+            {
+                var element = _locationElementHoldersPull[0];
+                _locationElementHoldersPull.RemoveAt(0); 
+                Destroy(element);
+            }
+        }
+    }
+}
