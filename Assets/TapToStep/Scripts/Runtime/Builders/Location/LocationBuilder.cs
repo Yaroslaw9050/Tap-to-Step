6271b1d6e828bf3e35f4e2264eb56cb6c6ccb33e
Yaroslav Homziak <ywgk0102@gmail.com>
2025-01-25 07:19:08 +0200

    feat (background generation) : added background gen. to first spawns

--- Assets/TapToStep/Scripts/Runtime/Builders/Location/LocationBuilder.cs
+++ Assets/TapToStep/Scripts/Runtime/Builders/Location/LocationBuilder.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using Runtime.Builders.Location;
 using SO.Location.Logic;
@@ -10,13 +11,19 @@ namespace Runtime.Service.LocationGenerator
 {
     public class LocationBuilder: MonoBehaviour, ILocationGenerator
     {
-        [Header("Locations")]
+        [Header("Locations")] 
+        [SerializeField] private GameObject _backgroundPrefab;
         [SerializeField] private List<SwitchedLocationSO> _supportedLocationPull;
         [SerializeField] private Transform _locationParent;
         
-        private List<GameObject> _locationElementHoldersPull;
+        private readonly List<GameObject> r_locationElementHoldersPull = new();
+        private readonly List<GameObject> r_backgroundElementHoldersPull = new();
+        
         private Vector3 _locationGenerationPoint;
         private bool _isFirstGeneration = true;
+        private int _backgroundCounter;
+        
+        private const int BACKGROUND_OFFSET = 1000;
 
         private void Start()
         {
@@ -30,6 +37,7 @@ namespace Runtime.Service.LocationGenerator
             if (_isFirstGeneration)
             {
                 _isFirstGeneration = !_isFirstGeneration;
+                await CreateBackgroundAsync();
                 await CreateLocationAsync(_supportedLocationPull[0], 20);
                 await CreateLocationAsync(_supportedLocationPull[randomLocationIndex], 20);
             }
@@ -44,7 +52,7 @@ namespace Runtime.Service.LocationGenerator
         {
             var elementQueue = locationSo.GetLocations();
             var startLocationSpawnPosition = _locationGenerationPoint;
-            var locationElementsHolder = new GameObject($"{nameof(locationSo)}");
+            var locationElementsHolder = new GameObject($"{locationSo.name}");
             locationElementsHolder.transform.SetParent(_locationParent);
             
             while (elementQueue.Count > 0)
@@ -59,19 +67,29 @@ namespace Runtime.Service.LocationGenerator
                         startLocationSpawnPosition.y, 
                         startLocationSpawnPosition.z + locationSo.LocationOffset);
             }
-            
-            _locationElementHoldersPull.Add(locationElementsHolder);
+            r_locationElementHoldersPull.Add(locationElementsHolder);
             _locationGenerationPoint = startLocationSpawnPosition;
         }
 
         private void RemoveOldLocation()
         {
-            if (_locationElementHoldersPull.Count > 3)
+            if (r_locationElementHoldersPull.Count > 3)
             {
-                var element = _locationElementHoldersPull[0];
-                _locationElementHoldersPull.RemoveAt(0); 
+                var element = r_locationElementHoldersPull[0];
+                r_locationElementHoldersPull.RemoveAt(0); 
                 Destroy(element);
             }
+        }
+
+        private async UniTask CreateBackgroundAsync()
+        {
+            if(_backgroundPrefab == null) return;
+            
+            var spawnPosition = Vector3.forward * (BACKGROUND_OFFSET * _backgroundCounter);
+            _backgroundCounter++;
+            var temp = Instantiate(_backgroundPrefab, spawnPosition, Quaternion.identity);
+            r_backgroundElementHoldersPull.Add(temp);
+            await UniTask.NextFrame();
         }
     }
 }
