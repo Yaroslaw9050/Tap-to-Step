8c25f84480d3128a8e810f0080d59ab4e965e134
Yaroslav Homziak <ywgk0102@gmail.com>
2025-03-01 09:08:01 +0200

    feat (view installer) : added view installer with bind all view model

--- Assets/TapToStep/Scripts/Core/Installers/ViewModelInstaller.cs
+++ Assets/TapToStep/Scripts/Core/Installers/ViewModelInstaller.cs
@@ -1,29 +1,20 @@
-using Patterns.Models;
+using Patterns.MVVM.ViewModels;
 using Patterns.ViewModels;
 using TapToStep.Scripts.Core.Service.ViewStorage;
-using UnityEngine;
 using Zenject;
 
 namespace Core.Installers
 {
     public class ViewModelInstaller: MonoInstaller
     {
-
         public override void InstallBindings()
         {
             BindViewStorageService();
-            
             Container.Bind<LoadingViewModel>().AsSingle();
             Container.Bind<TutorialViewModel>().AsSingle();
             Container.Bind<GameViewModel>().AsSingle();
             Container.Bind<DeadViewModel>().AsSingle();
-
-            LoadAllViewModelToStorage();
-        }
-
-        private void LoadAllViewModelToStorage()
-        {
-            
+            Container.Bind<MainMenuViewModel>().AsSingle();
         }
         
         private void BindViewStorageService()
