b0c9040a7796c0ceb5df749c5c98c65be42df51a
Yaroslav Homziak <ywgk0102@gmail.com>
2025-03-14 09:00:25 +0200

    chore (rounded distance value) : rounded current distance in SetCurrentDistance method

--- Assets/TapToStep/Scripts/Core/Service/LocalUser/LocalPlayerService.cs
+++ Assets/TapToStep/Scripts/Core/Service/LocalUser/LocalPlayerService.cs
@@ -38,12 +38,20 @@ namespace Core.Service.LocalUser
         
         public void SetCurrentDistance(double currentDistance)
         {
-            PlayerModel.CurrentDistance.Value = currentDistance;
+            var roundValue = Math.Round(currentDistance, 1);
+            PlayerModel.CurrentDistance.Value = roundValue;
+        }
+
+        public void ResetCurrentDistance()
+        {
+            _remoteDataStorageService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.CURRENT_DISTANCE_KEY, 0.0).Forget();
+            PlayerModel.CurrentDistance.Value = 0;
         }
         
         public void SetBestDistance(double bestDistance)
         {
-            PlayerModel.BestDistance.Value = bestDistance;
+            var roundValue = Math.Round(bestDistance, 1);
+            PlayerModel.BestDistance.Value = roundValue;
         }
 
         public void ForceUpdateDistanceDataOnRemote()
@@ -55,7 +63,7 @@ namespace Core.Service.LocalUser
         {
             if(PlayerModel.Bits.Value < cost) return false;
             
-            await _remoteDataStorageService.SaveBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.USER_NAME_KEY, userName);
+            await _remoteDataStorageService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.USER_NAME_KEY, userName);
             await _leaderboardService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.USER_NAME_KEY,userName);
             PlayerModel.UserName.Value = userName;
             RemoveBits(cost);
@@ -65,18 +73,18 @@ namespace Core.Service.LocalUser
         public void AddBits(ushort newValue)
         {
             PlayerModel.Bits.Value += newValue;
-            _remoteDataStorageService.SaveBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BITS_KEY, PlayerModel.Bits
+            _remoteDataStorageService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BITS_KEY, PlayerModel.Bits
                 .Value.ToString());
         }
 
         public void RemoveBits(uint removedValue)
         {
             PlayerModel.Bits.Value -= removedValue;
-            _remoteDataStorageService.SaveBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BITS_KEY, PlayerModel.Bits
+            _remoteDataStorageService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BITS_KEY, PlayerModel.Bits
                 .Value.ToString());
         }
 
-        public void AddDistance(float distance)
+        public void AddDistance(double distance)
         {
             var newDistance = Math.Round(distance, 1);
             PlayerModel.CurrentDistance.Value += newDistance;
@@ -91,22 +99,21 @@ namespace Core.Service.LocalUser
 
         private void UpdateDistanceDataOnRemote()
         {
-            _remoteDataStorageService.SaveBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.CURRENT_DISTANCE_KEY, 
-                PlayerModel.CurrentDistance.Value.ToString(CultureInfo.InvariantCulture));
+            _remoteDataStorageService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.CURRENT_DISTANCE_KEY, PlayerModel.CurrentDistance.Value.ToString(CultureInfo.InvariantCulture));
             UpdateBestDistance(PlayerModel.CurrentDistance.Value);
         }
 
-        public float GetStepLenght()
+        public double GetStepLenght()
         {
             return _startupPlayerSetting.StepLenght + _playerPerkSystem.GetPerkValueByType(PerkType.StepLenght);
         }
 
-        public float GetStepTime()
+        public double GetStepTime()
         {
             return _startupPlayerSetting.StepSpeed - _playerPerkSystem.GetPerkValueByType(PerkType.StepSpeed);
         }
 
-        public float GetTurnSpeed()
+        public double GetTurnSpeed()
         {
             return _playerPerkSystem.GetPerkValueByType(PerkType.TurnSpeed);
         }
@@ -120,23 +127,24 @@ namespace Core.Service.LocalUser
         {
             var userName = await _remoteDataStorageService.LoadBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.USER_NAME_KEY);
             var bits = await _remoteDataStorageService.LoadBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BITS_KEY);
-            var currentDistance = await _remoteDataStorageService.LoadBaseUserDataAsync(PlayerModel.UserId.Value, 
-                DatabaseKeyAssets.CURRENT_DISTANCE_KEY);
+            var currentDistance = await _remoteDataStorageService.LoadBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.CURRENT_DISTANCE_KEY);
+
+            if (double.TryParse(currentDistance, out var convertedDistance) == false) return;
+            if (ulong.TryParse(bits, out var convertedBits) == false) return;
+            if (string.IsNullOrEmpty(userName)) return;
             
-            SetCurrentDistance(double.Parse(currentDistance));
-            PlayerModel.Bits.Value = ushort.Parse(bits);
+            SetCurrentDistance(convertedDistance);
+            PlayerModel.Bits.Value = convertedBits;
             PlayerModel.UserName.Value = userName;
         }
 
         private void UpdateBestDistance(double currentDistance)
         {
-            if (!(currentDistance > PlayerModel.BestDistance.Value)) return;
+            if ((currentDistance > PlayerModel.BestDistance.Value) == false) return;
             
             PlayerModel.BestDistance.Value = currentDistance;
-            _leaderboardService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BEST_DISTANCE_KEY, currentDistance.ToString(CultureInfo
-                .InvariantCulture));
-            _remoteDataStorageService.SaveBaseUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BEST_DISTANCE_KEY, currentDistance.ToString
-                (CultureInfo.InvariantCulture));
+            _leaderboardService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BEST_DISTANCE_KEY, currentDistance.ToString(CultureInfo.InvariantCulture));
+            _remoteDataStorageService.SaveUserDataAsync(PlayerModel.UserId.Value, DatabaseKeyAssets.BEST_DISTANCE_KEY, currentDistance.ToString(CultureInfo.InvariantCulture));
         }
     }
 }
